a2f8c3caa57156c34a9fab46638ffde498576bd0
Dario Castañé <i@dario.im>
2019-08-12 23:23:12 +0200

    Integrant Tailwind i configurant VS Code

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `fetes.cat`,
+    description: `Els catalans fem coses i aquí les mostrem totes`,
+    author: `@im_dario`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
